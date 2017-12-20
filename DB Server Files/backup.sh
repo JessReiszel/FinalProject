a5d6fe254d875cb585808abeced1b52c4a842326
JessReiszel <32528428+JessReiszel@users.noreply.github.com>
2017-12-20 01:25:59 -0500

    Create backup.sh
    backup script to keep 2 days of backups at a time

---
+++ DB Server Files/backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+#Database info
+username="root"
+password="creative"
+host="localhost"
+db_name="company_inventory"
+
+backup_path="/home/sql_backup/sqldata"
+datewritten=$(date +"%b-%d-%T")
+
+
+#Sets default file permission
+umask 177
+
+#Dump the database into an SQL file
+mysqldump --user=$username --password=$password --host=$host $db_name > $backup_path/$db_name-$datewritten.sql
+
+#Find backup files in the sqldata folder older than 2 days and delete them
+find $backup_path/* -mtime +2 -exec rm {} \;
+
+                                
